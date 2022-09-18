ecc166bd28d3e7ee184cc640588fef1627de084b
Ranjan Mohan <ranjanmj@gmail.com>
2022-09-18 12:08:54 -0400

    Added latest version info for all repositories

---
+++ github/repository.py
@@ -0,0 +1,28 @@
+import json
+import os
+import requests
+
+
+class GitHubRepository:
+
+    def __init__(self, server: str, organization_name: str, name: str, additional_info: dict = dict()) -> None:
+        self.server = server
+        self.organization = organization_name
+        self.name = name
+        self.additional_info = additional_info
+
+    def get_repository_community_profile(self) -> dict:
+        request = requests.get(
+            f'{self.server}/repos/{self.organization}/{self.name}/community/profile',
+            headers={'Authorization': f'token {os.environ.get("GITHUB_TOKEN")}'}
+        )
+        community_profile: dict = json.loads(request.content)
+        return community_profile
+
+    def get_repository_latest_version(self) -> dict:
+        request = requests.get(
+            f'{self.server}/repos/{self.organization}/{self.name}/releases/latest',
+            headers={'Authorization': f'token {os.environ.get("GITHUB_TOKEN")}'}
+        )
+        latest_version: dict = json.loads(request.content)
+        return latest_version
--- organization_report.py
+++ organization_report.py
@@ -1,36 +1,23 @@
 import datetime
-import json
 import os
 
-import requests
 from mdutils import MdUtils
 from typing import List
+
+from github.organization import GitHubOrganization
 
 ORGANIZATION = 'padaiyal'
 GITHUB_INSTANCE = 'https://api.github.com'
 GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_TOKEN")
 
-
-def get_repositories() -> list:
-    request = requests.get(
-        f'{GITHUB_INSTANCE}/orgs/{ORGANIZATION}/repos',
-        headers={'Authorization': f'token {GITHUB_ACCESS_TOKEN}'}
-    )
-    repositories: list = json.loads(request.content)
-    return repositories
+github_organization = GitHubOrganization(
+    server='https://api.github.com',
+    name='padaiyal'
+)
 
 
 def get_organization_report_name() -> str:
     return "README"
-
-
-def get_repository_community_profile(repository_name: str) -> dict:
-    request = requests.get(
-        f'{GITHUB_INSTANCE}/repos/{ORGANIZATION}/{repository_name}/community/profile',
-        headers={'Authorization': f'token {GITHUB_ACCESS_TOKEN}'}
-    )
-    community_profile: dict = json.loads(request.content)
-    return community_profile
 
 
 def get_boolean_representation(value) -> str:
@@ -42,14 +29,16 @@ def get_boolean_representation(value) -> str:
 
 
 def generate_organization_report() -> None:
-    repositories: list = get_repositories()
+    repositories: tuple = github_organization.get_repositories()
     organization_report_name: str = get_organization_report_name()
-    table_headers: List[str] = ["Name", "Language", "Exposure", "Supported?", "Last Updated", "Open Issues", "License",
+    table_headers: List[str] = ["Name", "Latest version", "Language", "Exposure", "Supported?", "Last Updated",
+                                "Open Issues", "License",
                                 "Health %", "Description", "Content reports enabled?", "Code of Conduct?",
                                 "Contributing Guide?", "Issue template?", "Pull request template?", "README?", "Stars"]
     table: List[str] = list(table_headers)
     for repository in repositories:
-        community_profile: dict = get_repository_community_profile(repository['name'])
+        print(repository.name)
+        community_profile: dict = repository.get_repository_community_profile()
         community_profile['health_percentage'] = str(community_profile.get('health_percentage', '‍🤷'))
         community_profile['health_percentage'] += '%' if community_profile['health_percentage'] != '‍🤷' else ''
         community_profile['description'] = str(community_profile.get('description', '‍🤷'))
@@ -63,24 +52,28 @@ def generate_organization_report() -> None:
                 'readme': '🤷‍'
             }
         )
+        latest_release_info: dict = repository.get_repository_latest_version()
+        latest_release: str = f"[{latest_release_info.get('name', '🤷‍')}]({latest_release_info.get('html_url')})"
+        repository_additional_info: dict = repository.additional_info
         table.extend(
             [
-                f"[{repository['full_name']}]({repository['html_url']})",
-                repository['language'],
-                '🔒' if bool(repository['private']) else '🌏',
-                get_boolean_representation(not bool(repository['archived'])),
-                repository['updated_at'],
-                f"[{repository['open_issues_count']}]({repository['html_url']}/issues)",
-                repository['license']['name'] if repository['license'] else '🤷‍',
+                f"[{repository_additional_info['full_name']}]({repository_additional_info['html_url']})",
+                latest_release,
+                repository_additional_info['language'],
+                '🔒' if bool(repository_additional_info['private']) else '🌏',
+                get_boolean_representation(not bool(repository_additional_info['archived'])),
+                repository_additional_info['updated_at'],
+                f"[{repository_additional_info['open_issues_count']}]({repository_additional_info['html_url']}/issues)",
+                repository_additional_info['license']['name'] if repository_additional_info['license'] else '🤷‍',
                 community_profile['health_percentage'],
-                get_boolean_representation(community_profile['description']),
+                community_profile['description'],
                 get_boolean_representation(community_profile.get('content_reports_enabled', '🤷‍')),
                 get_boolean_representation(community_profile_files['code_of_conduct']),
                 get_boolean_representation(community_profile_files['contributing']),
                 get_boolean_representation(community_profile_files['issue_template']),
                 get_boolean_representation(community_profile_files['pull_request_template']),
                 get_boolean_representation(community_profile_files['readme']),
-                repository['stargazers_count']
+                repository_additional_info['stargazers_count']
             ]
         )
 
@@ -90,6 +83,14 @@ def generate_organization_report() -> None:
     md_file.new_line()
     md_file.new_table(columns=len(table_headers), rows=len(repositories) + 1, text=table, text_align='center')
     md_file.create_md_file()
+
+
+def generate_repository_documentation_report() -> None:
+    pass
+
+
+def generate_repository_health_report() -> None:
+    pass
 
 
 if __name__ == '__main__':
